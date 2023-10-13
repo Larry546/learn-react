4deed9f33ddb2efd23d249e4c1d2302de39ef148
Haonan Lu <hnlu546@gmail.com>
2023-10-12 20:23:49 -0400

    Defining Components 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import Profile from "./qcomps/profile_mistake.js";
+import Firstcomp from "./qcomps/firstcomp.js";
 
 export default function Home() {
   return (
     <div className={styles.main}>
       <Profile />
+      <Firstcomp />
     </div>
   );
 }
