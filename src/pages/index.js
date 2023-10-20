63fc01fd1f8fe329ebb1451496c5b7db709ba5f7
Haonan Lu <hnlu546@gmail.com>
2023-10-19 20:13:27 -0400

    state

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,17 +6,19 @@ import Profile from "./qcomps/profile_mistake.js";
 import Firstcomp from "./qcomps/firstcomp.js";
 import Bio from "./qcomps/bios.js";
 import TodoList from "./qcomps/todos.js";
-import Gallery from "./qcomps/gallery_props.js";
+// import Gallery from "./qcomps/gallery_props.js";
 import Square from "./components/square.js";
 import PackingList from "./components/props_item.js";
 import List from "./qcomps/list_keys_id.js";
 import RecipeList from "./qcomps/recipes.js";
 import MenuBar from "./qcomps/menuBar.js";
+import Gallery from "./qcomps/state.js";
 
 export default function Home() {
   return (
     <div className={styles.main}>
-      <MenuBar />
+      <Gallery />
+      <Gallery />
     </div>
   );
 }
